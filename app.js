1e81a838c4e5c3effb1b8096a46869f6a73f8d46
manijangid78 <manijangid78@gmail.com>
2020-07-07 15:30:38 +0530

    Level-1 Authentication - Simple Authentication

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //jshint esversion:6
-require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,7 +17,10 @@ const userSchema = new mongoose.Schema({
   password : String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET,  encryptedFields: ["password"]});
+const secrateKey = "It'sManiHere";
+const signinKey = "ManishJangid"
+
+//userSchema.plugin(encrypt, {secret:secrateKey, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
