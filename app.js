2b58bc7cd1386a84dd4ea332f412f1bec9e3e6dc
manijangid78 <manijangid78@gmail.com>
2020-07-07 15:33:48 +0530

    Level-2 Authentication - Encrypted Password

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-//const encrypt = require("mongoose-encryption");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
 const secrateKey = "It'sManiHere";
 const signinKey = "ManishJangid"
 
-//userSchema.plugin(encrypt, {secret:secrateKey, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret:secrateKey, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
