a32b8c7522d740338e6e89cb385793650b09612b
manijangid78 <manijangid78@gmail.com>
2020-07-09 16:48:29 +0530

    Level-5, Cookies and Sessions are included in there, passport, passport-local, passport-local-mongoose, express-session pakages are included

--- app.js
+++ app.js
@@ -4,24 +4,41 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
-// variables
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.use(session({
+  secret: "ItsManiHere",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect('mongodb://localhost:27017/userDB', { useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
   email : String,
   password : String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
@@ -36,45 +53,49 @@ app.get("/login", function(req, res){
 });
 
 app.get("/logout", function(req, res){
+  req.logout();
   res.redirect("/");
+});
+
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login")
+  }
 });
 
 app.post("/register", function(req, res){
 
-  // Added bcrypt hash function to store hash
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-    const newUser = new User({
-      email : req.body.username,
-      password : hash
-    });
-
-    newUser.save(function(err){
-      if(!err){
-        res.render("secrets");
-      }else{
-        res.render(err);
-      }
-    });
+  // It's Nice thing don't require to store data in db it do automatically for us
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
   });
+
 });
 
 app.post("/login", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
 
-  User.find({email:username}, function(err, resultUser){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err){
     if(err){
       console.log(err);
+      res.redirect("/login");
     }else{
-
-      bcrypt.compare(password, resultUser[0].password, function(err, result){
-        if(result){
-          res.render("secrets");
-        }else{
-          res.redirect("/login");
-        }
+      passport.authenticate("local")(req, res, function(){
+          res.redirect("/secrets");
       });
-
     }
   });
 });
